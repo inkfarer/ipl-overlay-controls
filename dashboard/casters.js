f700e3b7dc309b5387944d4d4088f00e03f41037
inkfarer <inkfarer@inkfarer.com>
2021-02-02 01:13:14 +0200

    Updates to casters selection (breaks schema)

--- dashboard/casters.js
+++ dashboard/casters.js
@@ -1,10 +1,19 @@
 const casters = nodecg.Replicant('casters');
 
 casters.on('change', (newValue, oldValue) => {
-	document.querySelector('#castersContainer').innerHTML = '';
-	for (let i = 0; i < newValue.length; i++) {
-		const element = newValue[i];
-		createCasterElem(element.id, element, false);
+	console.log(newValue);
+	for (const id in newValue) {
+		const object = newValue[id];
+		updateOrCreateCreateCasterElem(id, object);
+	}
+
+	// Handle deletions
+	if (oldValue) {
+		for (const id in oldValue) {
+			if (!newValue[id]) {
+				deleteCasterElem(id);
+			}
+		}
 	}
 });
 
@@ -16,12 +25,33 @@ document.querySelector('#btnAddCaster').addEventListener('click', () => {
 	createCasterElem(generateId());
 });
 
+function deleteCasterElem(id) {
+	const container = document.querySelector(`#casterContainer_${id}`);
+	container.parentNode.removeChild(container);
+}
+
+function updateOrCreateCreateCasterElem(id, data = {name: '', twitter: '', pronouns: ''}) {
+	const container = document.querySelector(`#casterContainer_${id}`);
+	if (container) {
+		updateCasterElem(id, data);
+	} else {
+		createCasterElem(id, data, false);
+	}
+}
+
+function updateCasterElem(id, data = {name: '', twitter: '', pronouns: ''}) {
+	document.querySelector(`#casterName_${id}`).value = data.name
+	document.querySelector(`#casterTwitter_${id}`).value = data.twitter
+	document.querySelector(`#casterPronouns_${id}`).value = data.pronouns
+}
+
 function createCasterElem(id, data = {name: '', twitter: '', pronouns: ''}, newElem = true) {
 	var container = document.createElement('div');
 	container.classList.add('space');
 	container.id = `casterContainer_${id}`;
 
-	var elem = `<div class="selectContainer">
+	var elem = `
+	<div class="selectContainer">
 		<div class="inputLabel">Name</div>
 		<input type="text" id="casterName_${id}">
 	</div>
@@ -32,7 +62,7 @@ function createCasterElem(id, data = {name: '', twitter: '', pronouns: ''}, newE
 		</div>
 		<div class="selectContainer">
 			<div class="inputLabel">Pronouns</div>
-			<input type="text" id="casterPronoun_${id}">
+			<input type="text" id="casterPronouns_${id}">
 		</div>
 	</div>
 	<div class="horizontalLayout">
@@ -45,48 +75,27 @@ function createCasterElem(id, data = {name: '', twitter: '', pronouns: ''}, newE
 	// add data
 	document.querySelector(`#casterName_${id}`).value = data.name;
 	document.querySelector(`#casterTwitter_${id}`).value = data.twitter;
-	document.querySelector(`#casterPronoun_${id}`).value = data.pronouns;
+	document.querySelector(`#casterPronouns_${id}`).value = data.pronouns;
 
 	// remind to update
-	addInputChangeReminder([`casterName_${id}`, `casterTwitter_${id}`, `casterPronoun_${id}`], document.querySelector(`#updateCaster_${id}`));
+	addInputChangeReminder([`casterName_${id}`, `casterTwitter_${id}`, `casterPronouns_${id}`], document.querySelector(`#updateCaster_${id}`));
 
 	// button click event
 	document.querySelector(`#updateCaster_${id}`).addEventListener('click', e => {
-		var id = e.target.id.split('_')[1];
-		var index = getCasterIndex(id);
-
-		var newData = {
-			id: id,
+		const id = e.target.id.split('_')[1];
+		casters.value[id] = {
 			name: document.querySelector(`#casterName_${id}`).value,
 			twitter: document.querySelector(`#casterTwitter_${id}`).value,
-			pronouns: document.querySelector(`#casterPronoun_${id}`).value
-		}
-
-		if (index !== null) {
-			casters.value[index] = newData;
-		} else {
-			casters.value.push(newData);
-		}
+			pronouns: document.querySelector(`#casterPronouns_${id}`).value
+		};
 	});
 	document.querySelector(`#removeCaster_${id}`).addEventListener('click', e => {
-		var id = e.target.id.split('_')[1];
-		var index = getCasterIndex(id);
-		if (index) {
-			casters.value.splice(index, 1);
+		const id = e.target.id.split('_')[1];
+
+		if (casters.value[id]) {
+			delete casters.value[id];
 		} else {
-			// if value is unsaved to replicant
-			let container = document.querySelector(`#casterContainer_${id}`);
-			container.parentNode.removeChild(container);
+			deleteCasterElem(id);
 		}
 	});
-}
-
-function getCasterIndex(id) {
-	for (let i = 0; i < casters.value.length; i++) {
-		const element = casters.value[i];
-		if (element.id == id) {
-			return i;
-		}
-	}
-	return null;
 }
