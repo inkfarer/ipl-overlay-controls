1965c01378db6ae3cc51aaa5e89005ea06e8bb35
inkfarer <inkfarer@inkfarer.com>
2021-03-19 18:22:19 +0200

    move modes and stages list to global scripts

--- dashboard/rounds.js
+++ dashboard/rounds.js
@@ -2,45 +2,6 @@ const rounds = nodecg.Replicant('rounds');
 
 const activeRoundId = nodecg.Replicant('activeRoundId');
 
-const splatStages = [
-    'Ancho-V Games',
-    'Arowana Mall',
-    'Blackbelly Skatepark',
-    'Camp Triggerfish',
-    'Goby Arena',
-    'Humpback Pump Track',
-    'Inkblot Art Academy',
-    'Kelp Dome',
-    'MakoMart',
-    'Manta Maria',
-    'Moray Towers',
-    'Musselforge Fitness',
-    'New Albacore Hotel',
-    'Piranha Pit',
-    'Port Mackerel',
-    'Shellendorf Institute',
-    'Shifty Station',
-    'Snapper Canal',
-    'Starfish Mainstage',
-    'Sturgeon Shipyard',
-    'The Reef',
-    'Wahoo World',
-    'Walleye Warehouse',
-    'Skipper Pavilion',
-    'Unknown Stage',
-];
-splatStages.sort();
-
-const splatModes = [
-    'Clam Blitz',
-    'Tower Control',
-    'Rainmaker',
-    'Splat Zones',
-    'Turf War',
-    'Unknown Mode',
-];
-splatModes.sort();
-
 //perhaps a little overcomplicated but it will do
 function generateId() {
     return '' + Math.random().toString(36).substr(2, 9);
