2573441a7d12c0af2d98c42e206754fda712d5c4
Vincent Lee <vlee@vlee.me.uk>
2021-06-01 19:42:58 +0100

    fix index.js indenting

--- extension/index.js
+++ extension/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = function (nodecg) {
-    require('./music')(nodecg);
-    require('./gameWinnerSetter')(nodecg);
-    require('./tournamentImporter').listen(nodecg);
-    require('./roundImporter').listen(nodecg);
-    require('./fileImport')(nodecg);
+	require('./music')(nodecg);
+	require('./gameWinnerSetter')(nodecg);
+	require('./tournamentImporter').listen(nodecg);
+	require('./roundImporter').listen(nodecg);
+	require('./fileImport')(nodecg);
 	require('./radia').listen(nodecg);
 };
