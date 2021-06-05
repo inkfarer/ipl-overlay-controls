0acdcdb708a346b30b5ddc3590876cb931d39b97
inkfarer <inkfarer@inkfarer.com>
2021-06-05 17:11:32 +0300

    Replace tabs with spaces in extension; Simplify getLiveCommentators listener

--- extension/radia.js
+++ extension/radia.js
@@ -1,62 +1,51 @@
 const axios = require('axios').default;
 
 async function listen(nodecg) {
-	const casters = nodecg.Replicant('casters');
-	const radiaSettings = nodecg.Replicant('radiaSettings');
-	var apiUrl;
-	var authentication;
+    const casters = nodecg.Replicant('casters');
+    const radiaSettings = nodecg.Replicant('radiaSettings');
+    let apiUrl;
+    let authentication;
 
-	if (
-		!nodecg.bundleConfig ||
-		typeof nodecg.bundleConfig.radia === 'undefined'
-	) {
-		nodecg.log.error(
-			`"radia" is not defined in cfg/${nodecg.bundleName}.json! The ability to import data via the Radia
+    if (!nodecg.bundleConfig || typeof nodecg.bundleConfig.radia === 'undefined') {
+        nodecg.log.error(
+            `"radia" is not defined in cfg/${nodecg.bundleName}.json! The ability to import data via the Radia
 			Production API is not possible`
-		);
-		radiaSettings.value.enabled = false;
-		return;  // Exit from extension as keys aren't available to use.
-	} else {
-		apiUrl = nodecg.bundleConfig.radia.url;
-		authentication = nodecg.bundleConfig.radia.authentication;
-		radiaSettings.value.enabled = true;
-	}
+        );
+        radiaSettings.value.enabled = false;
+        return;
+    } else {
+        apiUrl = nodecg.bundleConfig.radia.url;
+        authentication = nodecg.bundleConfig.radia.authentication;
+        radiaSettings.value.enabled = true;
+    }
 
-	nodecg.listenFor('getLiveCommentators', async (data, ack) => {
-		getLiveCasters(apiUrl, authentication, radiaSettings.value.guildID)
-			.then((data) => {
-				var addedCasters = []  // stores casters added to replicant
-				// Clear all currently stored caster data
-				casters.value = {}
-				// Get how many casters we need to place into the replicant
-				var numberOfCaster = 3;
-				// If the API hands back less than 3 casters that the number of casters to assign
-				if(data.length < numberOfCaster){
-					numberOfCaster = data.length;
-				}
-				// Place data into replicant
-				for(var i = 0; i < numberOfCaster; i++){
-					const commentator = data.pop();
-					casters.value[commentator['discord_user_id']] = {
-						"twitter": commentator['twitter'],
-						"name": commentator['name'],
-						"pronouns": commentator['pronouns'],
-					};
-					addedCasters.push(commentator)
-				}
-				// Returns object where:
-				// add: The casters that where added to the replicant
-				// extra: Casters that where not added to replicant as we already meet the max of 3 casters
-				ack(null, {add: addedCasters, extra: data});
-			})
-			.catch((err) => {
-				// If the API gives us a 404, just ignore it :)
-				if(err.response.status === 404){
-					ack(null, null);
-				}
-				ack(err);
-			});
-	});
+    nodecg.listenFor('getLiveCommentators', async (data, ack) => {
+        getLiveCasters(apiUrl, authentication, radiaSettings.value.guildID)
+            .then((data) => {
+                const castersToAdd = data.slice(0, 3);
+                const extraCasters = data.slice(3);
+
+                // Format casters to the map format our replicant expects
+                casters.value = castersToAdd.reduce((map, obj) => {
+                    const id = obj.discord_user_id;
+                    delete obj.discord_user_id;
+                    map[id] = obj;
+                    return map;
+                }, {});
+
+                // Returns object where:
+                // add: The casters that where added to the replicant
+                // extra: Casters that where not added to replicant as we already meet the max of 3 casters
+                ack(null, { add: castersToAdd, extra: extraCasters });
+            })
+            .catch((err) => {
+                // If the API gives us a 404, just ignore it :)
+                if (err.response.status === 404) {
+                    ack(null, null);
+                }
+                ack(err);
+            });
+    });
 }
 
 /**
@@ -67,33 +56,36 @@ async function listen(nodecg) {
  * @returns {Promise<list>} List of live casters
  */
 async function getLiveCasters(url, Authorisation, guildID) {
-	return new Promise((resolve, reject) => {
-		axios.get(`${url}/live/guild/${guildID}`, {
-			headers: {
-				'Authorization': Authorisation
-			}
-		}).then((response) => {
-			const data = response.data;
-			if (data.error) {
-				reject(data.error);
-				return;
-			}
-			let casters = [];
-			data.forEach(function(item, index) {
-				casters.push({
-					discord_user_id: item['discord_user_id'],
-					name: item["name"],
-					twitter: `@${item["twitter"]}`,
-					pronouns: item["pronouns"]
-				})
-			});
-			resolve(casters);
-		}).catch((err) => {
-			reject(err);
-		});
-	});
+    return new Promise((resolve, reject) => {
+        axios
+            .get(`${url}/live/guild/${guildID}`, {
+                headers: {
+                    Authorization: Authorisation,
+                },
+            })
+            .then((response) => {
+                const data = response.data;
+                if (data.error) {
+                    reject(data.error);
+                    return;
+                }
+                let casters = [];
+                data.forEach(function (item, index) {
+                    casters.push({
+                        discord_user_id: item['discord_user_id'],
+                        name: item['name'],
+                        twitter: `@${item['twitter']}`,
+                        pronouns: item['pronouns'],
+                    });
+                });
+                resolve(casters);
+            })
+            .catch((err) => {
+                reject(err);
+            });
+    });
 }
 
 module.exports = {
-	listen
+    listen,
 };
