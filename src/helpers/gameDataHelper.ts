63497cfe2e96a509faf5ba71ce5516a66a49b2f6
inkfarer <inkfarer@inkfarer.com>
2021-07-22 03:32:56 +0300

    Remove unused helper

--- src/helpers/gameDataHelper.ts
+++
@@ -1,6 +0,0 @@
-import { GameWinner } from '../types/enums/gameWinner';
-import { GameData } from '../types/schemas';
-
-export function createEmptyGameData(size: number): GameData {
-    return Array(size).fill({ winner: GameWinner.NO_WINNER });
-}
