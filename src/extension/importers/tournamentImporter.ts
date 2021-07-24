9293f6df218d7ce506b657bcd666b127bc4ace9d
inkfarer <inkfarer@inkfarer.com>
2021-07-25 00:14:50 +0300

    Fix tournament importer inexplicably failing

--- src/extension/importers/tournamentImporter.ts
+++ src/extension/importers/tournamentImporter.ts
@@ -7,8 +7,8 @@ import { Team } from 'types/team';
 import { BattlefyTournamentData } from '../types/battlefyTournamentData';
 import { BracketType, BracketTypeHelper } from 'types/enums/bracketType';
 import { TournamentDataSource } from 'types/enums/tournamentDataSource';
-import { setActiveRoundTeams } from '../replicants/activeRoundHelper';
 import { setNextRoundTeams } from '../replicants/nextRoundHelper';
+import { setActiveRoundTeams } from '../replicants/activeRoundHelper';
 
 const nodecg = nodecgContext.get();
 
