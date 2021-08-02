08e9c85e50adbac033aaaa03a6dd386051aedd4d
inkfarer <inkfarer@inkfarer.com>
2021-08-03 01:53:04 +0300

    Test smashggClient

---
+++ src/extension/importers/clients/__tests__/smashggClient.test.ts
@@ -0,0 +1,238 @@
+import axios from 'axios';
+import { getSmashGGData } from '../smashggClient';
+import { TournamentDataSource } from 'types/enums/tournamentDataSource';
+import * as generateId from '../../../../helpers/generateId';
+describe('smashggClient', () => {
+    jest.mock('axios');
+    const mockPost = jest.fn();
+
+    beforeEach(() => {
+        jest.resetAllMocks();
+
+        axios.post = mockPost;
+    });
+
+    describe('getSmashGGData', () => {
+        it('fetches data from Smash.gg and turns it into our expected format', async () => {
+            jest.spyOn(generateId, 'generateId')
+                .mockReturnValueOnce('111111')
+                .mockReturnValueOnce('222222');
+
+            mockPost.mockResolvedValue({
+                data: {
+                    data: {
+                        tournament: {
+                            name: 'Cool Tournament',
+                            teams: {
+                                pageInfo: {
+                                    totalPages: 1
+                                },
+                                nodes: [
+                                    {
+                                        name: 'Team One',
+                                        entrant: {
+                                            participants: [
+                                                { gamerTag: 'Player One' },
+                                                { gamerTag: 'Player Two' }
+                                            ]
+                                        }
+                                    },
+                                    {
+                                        name: 'Team ???',
+                                        entrant: undefined
+                                    },
+                                    {
+                                        name: 'Team Two',
+                                        entrant: {
+                                            participants: [
+                                                { gamerTag: 'Player Three' },
+                                                { gamerTag: 'Player Four' }
+                                            ]
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+                    }
+                }
+            });
+
+            const result = await getSmashGGData('ggtourney', '149083257830574');
+
+            expect(result).toEqual({
+                meta: {
+                    id: 'ggtourney',
+                    name: 'Cool Tournament',
+                    source: TournamentDataSource.SMASHGG
+                },
+                teams: [
+                    {
+                        id: '111111',
+                        name: 'Team One',
+                        showLogo: true,
+                        players: [
+                            { name: 'Player One' },
+                            { name: 'Player Two' }
+                        ]
+                    },
+                    {
+                        id: '222222',
+                        name: 'Team Two',
+                        showLogo: true,
+                        players: [
+                            { name: 'Player Three' },
+                            { name: 'Player Four' }
+                        ]
+                    }
+                ]
+            });
+            expect(mockPost).toHaveBeenCalledWith(
+                'https://api.smash.gg/gql/alpha',
+                expect.any(String),
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Accept: 'application/json',
+                        Authorization: 'Bearer 149083257830574'
+                    }
+                }
+            );
+            expect(cleanUpGraphqlQuery(mockPost.mock.calls[0][1]))
+                .toEqual('{"query":"query Entrants($slug: String!, $page: Int!, $perPage: Int!) '
+                    + '{ tournament(slug: $slug) { id name teams(query: { page: $page perPage: $perPage })'
+                    + ' { pageInfo { total totalPages } nodes { id name entrant { id participants { id gam'
+                    + 'erTag } } } } } }","variables":{"slug":"ggtourney","page":1,"perPage":"50"}}');
+        });
+
+        it('fetches data multiple times if more than one page is available', async () => {
+            jest.spyOn(generateId, 'generateId')
+                .mockReturnValueOnce('111111')
+                .mockReturnValueOnce('222222')
+                .mockReturnValueOnce('333333');
+
+            mockPost.mockResolvedValueOnce({
+                data: {
+                    data: {
+                        tournament: {
+                            name: 'Cool Tournament',
+                            teams: {
+                                pageInfo: {
+                                    totalPages: 3
+                                },
+                                nodes: [
+                                    {
+                                        name: 'Team One',
+                                        entrant: {
+                                            participants: [
+                                                { gamerTag: 'Player One' },
+                                                { gamerTag: 'Player Two' }
+                                            ]
+                                        }
+                                    },
+                                ]
+                            }
+                        }
+                    }
+                }
+            }).mockResolvedValueOnce({
+                data: {
+                    data: {
+                        tournament: {
+                            name: 'Cool Tournament',
+                            teams: {
+                                pageInfo: {
+                                    totalPages: 3
+                                },
+                                nodes: [
+                                    {
+                                        name: 'Team ???',
+                                        entrant: undefined
+                                    },
+                                    {
+                                        name: 'Cool Team',
+                                        entrant: {
+                                            participants: [
+                                                { gamerTag: 'Player One' },
+                                                { gamerTag: 'Player Two' }
+                                            ]
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+                    }
+                }
+            }).mockResolvedValueOnce({
+                data: {
+                    data: {
+                        tournament: {
+                            name: 'Cool Tournament',
+                            teams: {
+                                pageInfo: {
+                                    totalPages: 3
+                                },
+                                nodes: [
+                                    {
+                                        name: 'Team Two',
+                                        entrant: {
+                                            participants: [
+                                                { gamerTag: 'Player Three' },
+                                                { gamerTag: 'Player Four' }
+                                            ]
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+                    }
+                }
+            });
+
+            const result = await getSmashGGData('ggtourney', '149083257830574');
+
+            expect(result).toEqual({
+                meta: {
+                    id: 'ggtourney',
+                    name: 'Cool Tournament',
+                    source: TournamentDataSource.SMASHGG
+                },
+                teams: [
+                    {
+                        id: '111111',
+                        name: 'Team One',
+                        showLogo: true,
+                        players: [
+                            { name: 'Player One' },
+                            { name: 'Player Two' }
+                        ]
+                    },
+                    {
+                        id: '222222',
+                        name: 'Cool Team',
+                        showLogo: true,
+                        players: [
+                            { name: 'Player One' },
+                            { name: 'Player Two' }
+                        ]
+                    },
+                    {
+                        id: '333333',
+                        name: 'Team Two',
+                        showLogo: true,
+                        players: [
+                            { name: 'Player Three' },
+                            { name: 'Player Four' }
+                        ]
+                    }
+                ]
+            });
+            expect(mockPost).toHaveBeenCalledTimes(3);
+        });
+    });
+});
+
+function cleanUpGraphqlQuery(query: string): string {
+    return query
+        .replace(/\\n/g, '')
+        .replace(/ {4,}/g, ' ');
+}
--- src/extension/importers/clients/smashggClient.ts
+++ src/extension/importers/clients/smashggClient.ts
@@ -5,47 +5,41 @@ import { TournamentData } from 'schemas';
 import { TournamentDataSource } from 'types/enums/tournamentDataSource';
 
 export async function getSmashGGData(slug: string, token: string): Promise<TournamentData> {
-    return new Promise((resolve, reject) => {
-        getSmashGGPage(1, slug, token, true)
-            .then(async data => {
-                const tourneyInfo: TournamentData = {
-                    meta: {
-                        id: slug,
-                        source: TournamentDataSource.SMASHGG,
-                        name: data.raw.data.tournament.name
-                    },
-                    teams: []
-                };
-                tourneyInfo.teams = tourneyInfo.teams.concat(data.pageInfo);
+    const firstPageData = await getSmashGGPage(1, slug, token, true);
 
-                // If there are more pages, add them to our data set
-                if (data.raw.data.tournament.teams.pageInfo.totalPages > 1) {
-                    const pagePromises = [];
-                    for (let i = 2; i <= data.raw.data.tournament.teams.pageInfo.totalPages; i++) {
-                        pagePromises.push(getSmashGGPage(i, slug, token));
-                    }
+    const tourneyInfo: TournamentData = {
+        meta: {
+            id: slug,
+            source: TournamentDataSource.SMASHGG,
+            name: firstPageData.raw.data.tournament.name
+        },
+        teams: []
+    };
+    tourneyInfo.teams = tourneyInfo.teams.concat(firstPageData.pageInfo);
 
-                    const pages = await Promise.all(pagePromises);
+    // If there are more pages, add them to our data set
+    if (firstPageData.raw.data.tournament.teams.pageInfo.totalPages > 1) {
+        const pagePromises = [];
+        for (let i = 2; i <= firstPageData.raw.data.tournament.teams.pageInfo.totalPages; i++) {
+            pagePromises.push(getSmashGGPage(i, slug, token));
+        }
 
-                    for (let i = 0; i < pages.length; i++) {
-                        tourneyInfo.teams = tourneyInfo.teams.concat(pages[i].pageInfo);
-                    }
-                }
+        const pages = await Promise.all(pagePromises);
 
-                resolve(tourneyInfo);
-            })
-            .catch(err => {
-                reject(err);
-            });
-    });
+        for (let i = 0; i < pages.length; i++) {
+            tourneyInfo.teams = tourneyInfo.teams.concat(pages[i].pageInfo);
+        }
+    }
+
+    return tourneyInfo;
 }
 
-export async function getSmashGGPage(
+async function getSmashGGPage(
     page: number,
     slug: string,
     token: string,
-    getRaw = false): Promise<{ pageInfo: Team[], raw?: AxiosResponse }> {
-
+    getRaw = false
+): Promise<{ pageInfo: Team[], raw?: AxiosResponse }> {
     const query = `query Entrants($slug: String!, $page: Int!, $perPage: Int!) {
         tournament(slug: $slug) {
         id
@@ -74,61 +68,51 @@ export async function getSmashGGPage(
     }`;
     const perPage = '50';
 
-    return new Promise((resolve, reject) => {
-        axios
-            .post(
-                'https://api.smash.gg/gql/alpha',
-                JSON.stringify({
-                    query,
-                    variables: {
-                        slug,
-                        page,
-                        perPage
-                    }
-                }),
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Accept: 'application/json',
-                        Authorization: `Bearer ${token}`
-                    }
-                }
-            )
-            .then(response => {
-                const { data } = response;
-                const pageInfo: Team[] = [];
-                for (let i = 0; i < data.data.tournament.teams.nodes.length; i++) {
-                    const teamPlayers = [];
-                    const element = data.data.tournament.teams.nodes[i];
+    const response = await axios.post(
+        'https://api.smash.gg/gql/alpha',
+        JSON.stringify({
+            query,
+            variables: {
+                slug,
+                page,
+                perPage
+            }
+        }),
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: `Bearer ${token}`
+            }
+        }
+    );
 
-                    if (!element.entrant) continue;
+    const { data } = response;
+    const pageInfo: Team[] = [];
+    for (let i = 0; i < data.data.tournament.teams.nodes.length; i++) {
+        const teamPlayers = [];
+        const element = data.data.tournament.teams.nodes[i];
 
-                    for (let j = 0; j < element.entrant.participants.length; j++) {
-                        const teamPlayer = element.entrant.participants[j];
-                        const name = teamPlayer.gamerTag;
-                        teamPlayers.push({ name });
-                    }
+        if (!element.entrant) continue;
 
-                    const teamName = element.name;
-                    pageInfo.push({
-                        id: generateId(),
-                        name: teamName,
-                        players: teamPlayers,
-                        showLogo: true
-                    });
-                }
+        for (let j = 0; j < element.entrant.participants.length; j++) {
+            const teamPlayer = element.entrant.participants[j];
+            const name = teamPlayer.gamerTag;
+            teamPlayers.push({ name });
+        }
 
-                if (getRaw) {
-                    resolve({
-                        pageInfo,
-                        raw: data
-                    });
-                } else {
-                    resolve({ pageInfo });
-                }
-            })
-            .catch(e => {
-                reject(e);
-            });
-    });
+        const teamName = element.name;
+        pageInfo.push({
+            id: generateId(),
+            name: teamName,
+            players: teamPlayers,
+            showLogo: true
+        });
+    }
+
+    if (getRaw) {
+        return { pageInfo, raw: data };
+    } else {
+        return { pageInfo };
+    }
 }
