4091c3970944983509556896893c66d970d4cd18
inkfarer <inkfarer@inkfarer.com>
2021-10-19 21:53:01 +0300

    Add missing button colors

--- src/dashboard/styles/colors.ts
+++ src/dashboard/styles/colors.ts
@@ -2,11 +2,14 @@ export const colors = {
     blue: '#6155BD',
     green: '#00A651',
     red: '#e74e36',
-    yellow: '#FDD835'
+    yellow: '#FDD835',
+    orange: '#FF682E'
 };
 
 export const buttonColors = {
     blue: colors.blue,
     green: colors.green,
-    red: colors.red
+    red: colors.red,
+    orange: colors.orange,
+    'green-success': '#18C682'
 };
