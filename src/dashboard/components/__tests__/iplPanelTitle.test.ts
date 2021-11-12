6ed3244f375333ea6b4b1dd164d5339de2f93a0d
inkfarer <inkfarer@inkfarer.com>
2021-11-12 23:14:26 +0200

    Add ipl-panel-title component

---
+++ src/dashboard/components/__tests__/iplPanelTitle.test.ts
@@ -0,0 +1,24 @@
+import IplPanelTitle from '../iplPanelTitle.vue';
+import { config, mount } from '@vue/test-utils';
+import { mockDialog, mockGetDialog } from '../../__mocks__/mockNodecg';
+
+describe('IplPanelTitle', () => {
+    config.global.mocks = {
+        FontAwesomeIcon: true
+    };
+
+    it('matches snapshot', () => {
+        const wrapper = mount(IplPanelTitle, { props: { title: 'Cool Dialog', dialogName: 'coolDialog' } });
+
+        expect(wrapper.html()).toMatchSnapshot();
+    });
+
+    it('closes dialog on close button click', () => {
+        const wrapper = mount(IplPanelTitle, { props: { title: 'Cool Dialog', dialogName: 'coolDialog' } });
+
+        wrapper.getComponent('[data-test="close-button"]').vm.$emit('click');
+
+        expect(mockGetDialog).toHaveBeenCalledWith('coolDialog');
+        expect(mockDialog.close).toHaveBeenCalledTimes(1);
+    });
+});
---
+++ src/dashboard/components/iplPanelTitle.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="ipl-panel-title layout horizontal center-vertical">
+        <span class="ipl-panel-title__title-text">{{ title }}</span>
+        <font-awesome-icon
+            icon="times"
+            class="close-icon"
+            data-test="close-button"
+            @click="closeDialog"
+        />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { NodecgDialog } from '../types/dialog';
+
+library.add(faTimes);
+
+export default defineComponent({
+    name: 'IplPanelTitle',
+
+    components: { FontAwesomeIcon },
+
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        dialogName: {
+            type: String,
+            required: true
+        }
+    },
+
+    setup(props) {
+        return {
+            closeDialog() {
+                (nodecg.getDialog(props.dialogName) as NodecgDialog).close();
+            }
+        };
+    }
+});
+</script>
+
+<style lang="scss" scoped>
+    .ipl-panel-title {
+        width: calc(100% - 16px);
+        height: 40px;
+        font-weight: 500;
+        font-size: 1.25em;
+        background-color: #262F40;
+        padding: 0 8px;
+        border-radius: 7px;
+
+        .ipl-panel-title__title-text {
+            flex-grow: 1;
+        }
+
+        .close-icon {
+            cursor: pointer;
+            margin-right: 8px;
+        }
+    }
+</style>
