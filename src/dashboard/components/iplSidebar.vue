8d8a9b4115d875c0164afad381d202dc69d0f2f3
inkfarer <inkfarer@inkfarer.com>
2021-11-13 17:14:33 +0200

    Add ipl-sidebar component

---
+++ src/dashboard/components/iplSidebar.vue
@@ -0,0 +1,107 @@
+<template>
+    <transition
+        name="sidebar-transition"
+        :duration="250"
+    >
+        <div
+            v-if="isOpen"
+            class="ipl-sidebar__wrapper"
+        >
+            <div
+                class="background"
+                @click="close"
+            />
+            <div class="content">
+                <slot />
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'IplSidebar',
+
+    props: {
+        isOpen: {
+            type: Boolean,
+            required: true
+        }
+    },
+
+    emits: ['update:isOpen'],
+
+    setup(props, { emit }) {
+        return {
+            close() {
+                emit('update:isOpen', false);
+            }
+        };
+    }
+});
+</script>
+
+<style lang="scss" scoped>
+.ipl-sidebar__wrapper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 100;
+
+    .background {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        z-index: 101;
+        background-color: rgba(0, 0, 0, 0.5);
+    }
+
+    .content {
+        width: 85%;
+        height: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        z-index: 102;
+        background-color: #2F3A4F;
+        padding: 8px;
+    }
+}
+
+.sidebar-transition-enter-active {
+    .background {
+        transition: opacity 250ms ease-out;
+    }
+
+    .content {
+        transition: left 250ms ease-out;
+    }
+}
+
+.sidebar-transition-leave-active {
+    .background {
+        transition: opacity 250ms ease-in;
+    }
+
+    .content {
+        transition: left 250ms ease-in;
+    }
+}
+
+.sidebar-transition-enter-from,
+.sidebar-transition-leave-to {
+    .background {
+        opacity: 0;
+    }
+
+    .content {
+        left: -85%;
+    }
+}
+</style>
