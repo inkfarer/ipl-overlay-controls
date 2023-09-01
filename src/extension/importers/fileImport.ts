e9447c82f47b9ed04a2727dc4ae248ecd0599e41
inkfarer <inkfarer@inkfarer.com>
2023-09-02 01:49:22 +0300

    Fix that odd damaged lockfile error

--- src/extension/importers/fileImport.ts
+++ src/extension/importers/fileImport.ts
@@ -10,21 +10,25 @@ const nodecg = nodecgContext.get();
 const runtimeConfig = nodecg.Replicant<RuntimeConfig>('runtimeConfig');
 const router = nodecg.Router();
 
+// Note that the following is a TypeScript *nightmare* as TS chooses to prefer the types from Multer (a dependency of
+// @types/nodecg) instead of express-fileupload, which is actually in use here. Therefore, we have to force TS to use
+// the correct types for req.files every step of the way here.
+
 router.post(
     '/upload-tournament-json',
     fileUpload({ limits: { fileSize: 50 * 1024 * 1024 } }),
     async (req, res) => {
         if (
             !req.files
-            || !req.files.file
+            || !(req.files as unknown as fileUpload.FileArray).file
             || !req.body.jsonType
-            || Array.isArray(req.files.file)
-            || req.files.file.mimetype !== 'application/json'
+            || Array.isArray((req.files as unknown as fileUpload.FileArray).file)
+            || ((req.files as unknown as fileUpload.FileArray).file as UploadedFile).mimetype !== 'application/json'
         ) {
             return res.status(400).send('Invalid attached file or jsonType property provided.');
         }
 
-        const file = req.files.file as UploadedFile;
+        const file = (req.files as unknown as fileUpload.FileArray).file as UploadedFile;
         const content = JSON.parse(file.data.toString());
 
         switch (req.body.jsonType) {
