1b9b29ea4fcf1943939e7e1ccaf253a00904c5cf
inkfarer <inkfarer@inkfarer.com>
2023-11-18 10:11:08 +0200

    When importing tournaments from a source not supporting match importing, hide the toggle to enable match importing instead of displaying a warning message

--- src/dashboard/nextMatch/__tests__/nextMatch.test.ts
+++ src/dashboard/nextMatch/__tests__/nextMatch.test.ts
@@ -3,6 +3,9 @@ import { config, mount } from '@vue/test-utils';
 import { useNextRoundStore } from '../../store/nextRoundStore';
 import { createTestingPinia, TestingPinia } from '@pinia/testing';
 import { IplSmallToggle } from '@iplsplatoon/vue-components';
+import { TournamentDataSource } from 'types/enums/tournamentDataSource';
+import difference from 'lodash/difference';
+import { useHighlightedMatchStore } from '../highlightedMatchStore';
 
 describe('NextMatch', () => {
     let pinia: TestingPinia;
@@ -17,6 +20,7 @@ describe('NextMatch', () => {
 
     beforeEach(() => {
         pinia = createTestingPinia();
+        config.global.plugins = [pinia];
 
         useNextRoundStore().$state = {
             // @ts-ignore
@@ -24,25 +28,19 @@ describe('NextMatch', () => {
                 showOnStream: true
             }
         };
+        // @ts-ignore
+        useHighlightedMatchStore().tournamentData = { meta: { source: TournamentDataSource.BATTLEFY } };
     });
 
     describe('template', () => {
         it('matches snapshot', () => {
-            const wrapper = mount(NextMatch, {
-                global: {
-                    plugins: [pinia]
-                }
-            });
+            const wrapper = mount(NextMatch);
 
             expect(wrapper.html()).toMatchSnapshot();
         });
 
         it('matches snapshot when choosing teams manually', async () => {
-            const wrapper = mount(NextMatch, {
-                global: {
-                    plugins: [pinia]
-                }
-            });
+            const wrapper = mount(NextMatch);
 
             wrapper.getComponent<typeof IplSmallToggle>('[data-test="choose-manually-toggle"]').vm.$emit('update:modelValue', true);
             await wrapper.vm.$nextTick();
@@ -51,14 +49,33 @@ describe('NextMatch', () => {
         });
     });
 
+    const importableSources = [
+        TournamentDataSource.SMASHGG,
+        TournamentDataSource.BATTLEFY
+    ];
+
+    it.each(difference(Object.values(TournamentDataSource), importableSources))('disables importing from source if the source is %s', source => {
+        // @ts-ignore
+        useHighlightedMatchStore().tournamentData = { meta: { source } };
+        const wrapper = mount(NextMatch);
+
+        const chooseManuallyToggle = wrapper.findComponent<typeof IplSmallToggle>('[data-test="choose-manually-toggle"]');
+        expect(chooseManuallyToggle.exists()).toEqual(false);
+    });
+
+    it.each(importableSources)('allows importing from source if the source is %s', source => {
+        // @ts-ignore
+        useHighlightedMatchStore().tournamentData = { meta: { source } };
+        const wrapper = mount(NextMatch);
+
+        const chooseManuallyToggle = wrapper.findComponent<typeof IplSmallToggle>('[data-test="choose-manually-toggle"]');
+        expect(chooseManuallyToggle.exists()).toEqual(true);
+    });
+
     it('handles changing show on stream checkbox', async () => {
         const store = useNextRoundStore();
         store.setShowOnStream = jest.fn();
-        const wrapper = mount(NextMatch, {
-            global: {
-                plugins: [pinia]
-            }
-        });
+        const wrapper = mount(NextMatch);
 
         wrapper.getComponent<typeof IplSmallToggle>('[data-test="show-on-stream-toggle"]').vm.$emit('update:modelValue', false);
         await wrapper.vm.$nextTick();
