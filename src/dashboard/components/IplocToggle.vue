3adf70a616b00adc592d1a589946dbe7b4d5ea73
inkfarer <inkfarer@inkfarer.com>
2024-04-07 17:43:01 +0300

    Add common components that are automatically translated

---
+++ src/dashboard/components/IplocToggle.vue
@@ -0,0 +1,17 @@
+<template>
+    <ipl-toggle
+        v-bind="$props"
+        :true-label="$t('common:largeToggle.show')"
+        :false-label="$t('common:largeToggle.hide')"
+    />
+</template>
+
+<script setup lang="ts">
+import { IplToggle } from '@iplsplatoon/vue-components';
+
+type BaseProps = (typeof IplToggle)['$props'];
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface Props extends /* @vue-ignore */ BaseProps { }
+
+defineProps<Props>();
+</script>
