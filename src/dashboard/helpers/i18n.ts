76f16865a30d31694835e2731bce3319e96f2e39
inkfarer <inkfarer@inkfarer.com>
2024-04-09 12:05:33 +0300

    Rework the way translations are loaded in the dashboard to make loading additional common translation files easier

--- src/dashboard/helpers/i18n.ts
+++ src/dashboard/helpers/i18n.ts
@@ -1,14 +1,27 @@
 import { RuntimeConfig } from 'schemas';
-import i18next from 'i18next';
-import type { Resource } from 'i18next/typescript/options';
-import { InterfaceLocale } from 'types/enums/InterfaceLocale';
+import i18next, { type BackendModule } from 'i18next';
 
-export async function initI18n(translationFileName: string): Promise<void> {
-    await i18next.init({
-        lng: 'en',
-        fallbackLng: 'en',
-        resources: await loadTranslations(translationFileName)
-    });
+const I18nBackend: BackendModule = {
+    type: 'backend',
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    init() {
+
+    },
+
+    async read(language, namespace) {
+        return loadTranslation(language, namespace);
+    },
+};
+
+export async function initI18n(defaultNS: string): Promise<void> {
+    await i18next
+        .use(I18nBackend)
+        .init({
+            lng: 'en',
+            fallbackLng: 'en',
+            ns: [defaultNS, 'common']
+        });
 
     const runtimeConfig = nodecg.Replicant<RuntimeConfig>('runtimeConfig');
     runtimeConfig.on('change', (newValue, oldValue) => {
@@ -25,18 +38,6 @@ export async function initI18n(translationFileName: string): Promise<void> {
             }
         }
     });
-}
-
-async function loadTranslations(name: string): Promise<Resource> {
-    const result: Resource = { };
-    for (const locale of Object.values(InterfaceLocale)) {
-        result[locale.toLowerCase()] = {
-            common: await loadTranslation(locale, 'common'),
-            translation: await loadTranslation(locale, name)
-        };
-    }
-
-    return result;
 }
 
 async function loadTranslation(locale: string, name: string) {
