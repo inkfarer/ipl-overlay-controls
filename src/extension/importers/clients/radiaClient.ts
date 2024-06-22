b22bcd1e1d86b100c63c855f52700ddad488be14
inkfarer <inkfarer@inkfarer.com>
2024-06-22 14:48:21 +0300

    Bump dependencies

--- src/extension/importers/clients/radiaClient.ts
+++ src/extension/importers/clients/radiaClient.ts
@@ -111,12 +111,12 @@ export async function createPrediction(guildId: string, data: CreatePrediction):
     }
 }
 
-export function handleAxiosError(e: AxiosError | Error): void {
+export function handleAxiosError(e: AxiosError<{ detail: unknown }> | Error): void {
     if ('response' in e) {
         let message = i18next.t('radiaClient.requestFailed', { statusCode: e.response.status });
         if (e.response.data?.detail) {
             if (typeof e.response.data.detail === 'object') {
-                if (e.response.data.detail.message) {
+                if ('message' in e.response.data.detail) {
                     message += `: ${e.response.data.detail.message}`;
                 } else {
                     message += `: ${JSON.stringify(e.response.data.detail)}`;
