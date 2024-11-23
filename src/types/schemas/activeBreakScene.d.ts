706d1ff56c94344cd4579619d3cbcd2a12019f5c
inkfarer <inkfarer@inkfarer.com>
2024-11-23 16:26:28 +0200

    Allow any string to be set as the active break scene
    This doesn't seem to immediately break anything, but I'll have to experiment further before I commit to this.

--- src/types/schemas/activeBreakScene.d.ts
+++ src/types/schemas/activeBreakScene.d.ts
@@ -5,4 +5,4 @@
  * and run json-schema-to-typescript to regenerate this file.
  */
 
-export type ActiveBreakScene = 'main' | 'stages' | 'teams';
+export type ActiveBreakScene = ('main' | 'stages' | 'teams') | string;
